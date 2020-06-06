c63635ad329b7348ec735a7e274c492f5c1cb201
RSlomp <14997127+rslomp@users.noreply.github.com>
2020-06-06 17:46:06 +0200

    Update MdbModel.java

--- src/MdbModel.java
+++ src/MdbModel.java
@@ -15,6 +15,13 @@ public class MdbModel {
         support = new PropertyChangeSupport(this);
     }
 
+    public void add_film(String title, int year, String country, int budget, ProductionCompany production_company, List<Actor> actors, List<Person> crew){
+        List<Film> films_new = this.films;
+        films_new.add(new Film(title, year, country, budget, production_company, actors, crew));
+        support.firePropertyChange("films", this.films, films_new);
+        this.films = films_new;
+    }
+
     public void add_actor(String firstname, String lastname, int dob_day, int dob_month, int dob_year){
         List<Actor> actors_new = this.actors;
         actors_new.add(new Actor(firstname, lastname, dob_day, dob_month, dob_year));
@@ -22,11 +29,18 @@ public class MdbModel {
         this.actors = actors_new;
     }
 
-    public void add_film(String title, int year, String country, int budget, ProductionCompany production_company, List<Actor> actors, List<Person> crew){
-        List<Film> films_new = this.films;
-        films_new.add(new Film(title, year, country, budget, production_company, actors, crew));
-        support.firePropertyChange("films", this.films, films_new);
-        this.films = films_new;
+    public void add_crew(String firstname, String lastname, int dob_day, int dob_month, int dob_year){
+        List<Person> crew_new = this.crew;
+        crew_new.add(new Person(firstname, lastname, dob_day, dob_month, dob_year));
+        support.firePropertyChange("crew", this.crew, crew_new);
+        this.crew = crew_new;
+    }
+
+    public void add_production_company(String name){
+        List<ProductionCompany> pr_comps_new = this.production_companies;
+        pr_comps_new.add(new ProductionCompany(name));
+        support.firePropertyChange("production_companies", this.production_companies, pr_comps_new);
+        this.production_companies = pr_comps_new;
     }
 
     public void addPropertyChangeListener(PropertyChangeListener pcl) {
