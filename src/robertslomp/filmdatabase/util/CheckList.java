564a2402fabce500425d83c709be85341455bf1d
RSlomp <14997127+RSlomp@users.noreply.github.com>
2020-10-06 11:43:54 +0200

    Updated CheckList.java
    All checkboxes are now stored in a list.
    Text from selected checkboxes can be retrieved.

--- src/robertslomp/filmdatabase/util/CheckList.java
+++ src/robertslomp/filmdatabase/util/CheckList.java
@@ -2,8 +2,11 @@ package robertslomp.filmdatabase.util;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckList extends JPanel {
+    private final List<JCheckBox> check_boxes = new ArrayList<>();
 
     public CheckList() {
         super(new GridLayout(0, 1)); //1 column, any number of rows...
@@ -12,15 +15,26 @@ public class CheckList extends JPanel {
     public void add_checkbox(String text, int x, int y, int width, int height){
         JCheckBox box = new JCheckBox(text);
         box.setBounds(x, y, width, height);
-        this.add(box);
+        this.check_boxes.add(box);
+        this.add(this.check_boxes.get(this.check_boxes.size()-1));
     }
 
     public void fill_list(String[] values){
         int x = 0;
         int y = 177;
-        for (int i = 0; i < values.length; i++){
-            add_checkbox(values[i], x, y, 170, 20);
+        for (String value : values) {
+            add_checkbox(value, x, y, 170, 20);
             y = y + 22;
         }
+    }
+
+    public List<String> get_selected_actor(){
+        List<String> selected = new ArrayList<>();
+        for(JCheckBox box : this.check_boxes){
+            if(box.isSelected()){
+                selected.add(box.getText());
+            }
+        }
+        return selected;
     }
 }
