c5bdcb9c87cb6367afe6c8242dfc64de61a21d31
RSlomp <14997127+RSlomp@users.noreply.github.com>
2020-10-06 12:20:04 +0200

    Updated CreateFilmView.java
    Can now create objects.

--- src/robertslomp/filmdatabase/view/CreateFilmView.java
+++ src/robertslomp/filmdatabase/view/CreateFilmView.java
@@ -1,51 +1,80 @@
 package robertslomp.filmdatabase.view;
 
+import robertslomp.filmdatabase.controller.Controller;
 import robertslomp.filmdatabase.model.Actor;
 import robertslomp.filmdatabase.model.MdbModel;
-import robertslomp.filmdatabase.model.ProductionCompany;
+import robertslomp.filmdatabase.util.CheckList;
 import robertslomp.filmdatabase.util.ElementsFactory;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.text.JTextComponent;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class CreateFilmView extends Frame implements WindowListener, ActionListener {
-    private String window_title;
+
+public class CreateFilmView extends View {
+    private final String window_title;
     public JFrame frame;
     private JTextField title_txt;
+    private JTextField year_txt;
     private JTextField budget_txt;
     private JComboBox<String> country_box;
-    private ProductionCompany[] companies;
-    private JComboBox<String> company_box;
-    private Actor[] actors;
-    private JPanel actors_check;
+    private final Actor[] actors;
+    private CheckList actors_check;
     private JButton execute_button;
     private final MdbModel model = new MdbModel();
-    //private final Controller controller = new robertslomp.mvc.Controller();
+    private final Controller controller = new Controller();
 
 
-    public CreateFilmView(String title){
+    public CreateFilmView(String title) {
         super(title);
-        //this.actors = controller.get_actors();
-        //this.companies = controller.get_companies();
+        this.window_title = title;
+        this.actors = controller.get_all_actors().toArray(new Actor[0]);
         initialize();
     }
 
-    private void initialize(){
+
+    public void create_object(String title, int year, String country, int budget, List<Actor> actors){
+        model.create_film(title, year, country, budget, actors);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        GetString get_string = JTextComponent::getText;
+        GetInt get_int = (JTextField txt) -> Integer.parseInt(txt.getText());
+        GetBoxItem get_box_item = (JComboBox<String> box) -> (String) box.getSelectedItem();
+
+        String title = get_string.get_value(title_txt);
+        int year = get_int.get_value(year_txt);
+        String country = get_box_item.get_value(country_box);
+        int budget = get_int.get_value(budget_txt);
+        List<Actor> actors = new ArrayList<>();
+        List<String> selected_actor_names = this.actors_check.get_selected_items();
+
+        for (String selected_name : selected_actor_names){
+            String[] names = selected_name.split(" ");
+            actors.addAll(this.controller.get_actor_by_name(names[0], names[1]));
+        }
+
+        create_object(title, year, country, budget, actors);
+    }
+
+
+    @Override
+    protected void initialize() {
         String[] countries = new String[]{"Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola",
                 "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia",
                 "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
-                "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana",
+                "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana",
                 "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria",
                 "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands",
                 "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands",
                 "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica",
                 "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti",
                 "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
-                "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland",
+                "Eritrea", "Estonia", "Ethiopia", "Faeroe Islands", "Falkland Islands (Malvinas)", "Fiji", "Finland",
                 "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories",
                 "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada",
                 "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti",
@@ -54,12 +83,12 @@ public class CreateFilmView extends Frame implements WindowListener, ActionListe
                 "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati",
                 "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan",
                 "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia",
-                "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "North Macedonia",
-                "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique",
-                "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of",
-                "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar",
-                "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand",
-                "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway",
+                "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Madagascar",
+                "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania",
+                "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of",
+                "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia",
+                "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua",
+                "Niger", "Nigeria", "Niue", "Norfolk Island", "North Macedonia", "Northern Mariana Islands", "Norway",
                 "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru",
                 "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania",
                 "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia",
@@ -74,11 +103,7 @@ public class CreateFilmView extends Frame implements WindowListener, ActionListe
                 "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu",
                 "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)",
                 "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"};
-        //String[] companies = companies_to_string(this.companies);
-        //String[] actors = actors_to_string(this.actors);
-        String[] companies = {"A", "B"};
-        String[] actors = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-                "S", "T", "U", "V", "W", "X", "Y", "Z"};
+        String[] actors = actors_to_string(this.actors);
 
         this.frame = ElementsFactory.gen_frame(this.window_title, 50, 50, 400, 400 + (25 * actors.length));
 
@@ -91,8 +116,8 @@ public class CreateFilmView extends Frame implements WindowListener, ActionListe
         JLabel country_label = ElementsFactory.gen_label("Country", 25, 105, 86, 14);
         frame.getContentPane().add(country_label);
 
-        JLabel prod_comp_label = ElementsFactory.gen_label("Production Company", 25, 142, 146, 14);
-        frame.getContentPane().add(prod_comp_label);
+        JLabel year_label = ElementsFactory.gen_label("Year", 25, 142, 86, 14);
+        frame.getContentPane().add(year_label);
 
         this.title_txt = ElementsFactory.gen_text_field(168, 28, 146, 20);
         frame.getContentPane().add(this.title_txt);
@@ -105,67 +130,35 @@ public class CreateFilmView extends Frame implements WindowListener, ActionListe
         this.country_box = ElementsFactory.gen_combo_box(countries, 168, 102, 170, 20);
         frame.getContentPane().add(this.country_box);
 
-        this.company_box = ElementsFactory.gen_combo_box(companies, 168, 140, 170, 20);
-        frame.getContentPane().add(this.company_box);
+        this.year_txt = ElementsFactory.gen_text_field(168, 139, 170, 20);
+        frame.getContentPane().add(this.year_txt);
 
         this.actors_check = ElementsFactory.gen_checkbox_list(actors, 168, 177, 80, (25 * actors.length));
         frame.getContentPane().add(this.actors_check);
 
     }
 
-    private String[] companies_to_string(ProductionCompany[] companies){
-        String[] company_names = new String[companies.length];
-        for (int i = 0; i < companies.length; i++){
-            company_names[i] = companies[i].get_name();
+
+    private String[] actors_to_string(Actor[] actors) {
+        String[] actor_names = new String[actors.length];
+        for (int i = 0; i < actors.length; i++) {
+            actor_names[i] = actors[i].get_fullname();
         }
-        return company_names;
+        return actor_names;
     }
+}
 
-    private String[] actors_to_string(Actor[] actors){
-        String[] company_names = new String[actors.length];
-        for (int i = 0; i < actors.length; i++){
-            company_names[i] = actors[i].get_fullname();
-        }
-        return company_names;
-    }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
-    }
+interface GetString {
+    String get_value(JTextField txt);
+}
 
-    @Override
-    public void windowOpened(WindowEvent e) {
 
-    }
+interface GetInt {
+    int get_value(JTextField txt);
+}
 
-    @Override
-    public void windowClosing(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowIconified(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowActivated(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) {
-
-    }
+interface GetBoxItem {
+    String get_value(JComboBox<String> box);
 }
