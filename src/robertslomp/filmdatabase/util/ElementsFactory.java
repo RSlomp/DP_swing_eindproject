874989a76b8602289924125f1292bc4c0c697a1f
RSlomp <14997127+RSlomp@users.noreply.github.com>
2020-10-07 14:44:16 +0200

    Updated ElementsFactory.java
    - Added JavaDoc
    - Fixed X/Y coordinates of the checklist.

--- src/robertslomp/filmdatabase/util/ElementsFactory.java
+++ src/robertslomp/filmdatabase/util/ElementsFactory.java
@@ -3,7 +3,21 @@ package robertslomp.filmdatabase.util;
 import javax.swing.*;
 import java.awt.*;
 
+
+/**
+ * Factory Pattern for UI elements.
+ */
 public class ElementsFactory {
+    /**
+     * Creates a new frame to specification.
+     * @param title Frame title.
+     * @param x X-coordinate of the frame.
+     * @param y Y-coordinate of the frame.
+     * @param width Width of the frame.
+     * @param height Height of the frame.
+     * @return Created frame.
+     * @see JFrame
+     */
     public static JFrame gen_frame(String title, int x, int y, int width, int height){
         JFrame frame = new JFrame(title);
         frame.setBounds(x, y, width, height);
@@ -13,6 +27,16 @@ public class ElementsFactory {
     }
 
 
+    /**
+     * Creates a new label to specification.
+     * @param text The text to be displayed.
+     * @param x X-coordinate of the label.
+     * @param y Y-coordinate of the label.
+     * @param width Width of the label.
+     * @param height Height of the label.
+     * @return Created label.
+     * @see JLabel
+     */
     public static JLabel gen_label(String text, int x, int y, int width, int height){
         JLabel label = new JLabel(text);
         label.setBounds(x, y, width, height);
@@ -20,6 +44,15 @@ public class ElementsFactory {
     }
 
 
+    /**
+     * Creates a new text field to specifications.
+     * @param x X-coordinate of the text field.
+     * @param y Y-coordinate of the text field.
+     * @param width Width of the text field.
+     * @param height Height of the text field.
+     * @return Created text field.
+     * @see JTextField
+     */
     public static JTextField gen_text_field(int x, int y, int width, int height){
         JTextField text_field = new JTextField();
         text_field.setBounds(x, y, width, height);
@@ -27,6 +60,16 @@ public class ElementsFactory {
     }
 
 
+    /**
+     * Creates a new combo-box to specifications.
+     * @param values Array of values to be displayed in the combo-box.
+     * @param x X-coordinate of the combo-box.
+     * @param y Y-coordinate of the combo-box.
+     * @param width Width of the combo-box.
+     * @param height Height of the combo-box.
+     * @return Created combo-box.
+     * @see JComboBox
+     */
     public static JComboBox<String> gen_combo_box(String[] values, int x, int y, int width, int height){
         JComboBox<String> box = new JComboBox<>(values);
         box.setBounds(x, y, width, height);
@@ -34,11 +77,21 @@ public class ElementsFactory {
     }
 
 
+    /**
+     * Creates a new checklist to specifications.
+     * @param values Array of values to be displayed in the checklist.
+     * @param x X-coordinate of the checklist.
+     * @param y Y-coordinate of the checklist.
+     * @param width Width of the checklist.
+     * @param height Height of the checklist.
+     * @return Created Checklist.
+     * @see CheckList
+     */
     public static CheckList gen_checkbox_list(String[] values, int x, int y, int width, int height){
         //JPanel mainPanel = new JPanel();
         CheckList list = new CheckList();
         list.fill_list(values);
-        list.setBounds(0, 0, width, height);
+        list.setBounds(x, y, width, height);
         /*JPanel newPanel = new JPanel();
         newPanel.add(list);
         newPanel.setBounds(0, 0, width+10, height+10);
@@ -50,6 +103,16 @@ public class ElementsFactory {
     }
 
 
+    /**
+     * Creates a new button to specification.
+     * @param text The text to be displayed on the button.
+     * @param x X-coordinate of the button.
+     * @param y Y-coordinate of the button.
+     * @param width Width of the button.
+     * @param height Height of the button.
+     * @return Created button.
+     * @see JButton
+     */
     public static JButton gen_button(String text, int x, int y, int width, int height){
         JButton button = new JButton(text);
         button.setBackground(Color.GRAY);
